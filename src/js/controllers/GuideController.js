f409cc7c70c66cd6e63e47e28c3ce5c4b2305b17
Paul Thiel <lordnoname@t-online.de>
2016-02-04 23:04:17 +0100

    feat(): add unfinished mdcollapsedmenu and mdcollapsedmenuitem

--- src/js/controllers/GuideController.js
+++ src/js/controllers/GuideController.js
@@ -5,7 +5,7 @@ function GuideController($scope, $routeParams, $location, GuideService) {
 
   var guides;
   var id = $routeParams.id;
-
+	
   GuideService.fetchData().then(function(data) {
     guides = data;
     loadGuide();
@@ -19,11 +19,10 @@ function GuideController($scope, $routeParams, $location, GuideService) {
       var guide = guides[id];
       $scope.title = guide.title;
       $scope.path = 'content/guides/' + guide.path;
+      $scope.submenu = guide.submenu;
     } else {
       $scope.title = "Fehler";
       $scope.error = "Die gesuchte Anleitung konnte nicht in unserer Datenbank gefunden werden.";
     }
   }
-
-
 }
---
+++ src/js/directives/mdCollapsedMenu.ts
@@ -0,0 +1,8 @@
+﻿angular.module('wgHilfe')
+  .directive('mdCollapsedMenuItem', function () {
+    return {
+      restrict: 'AE',
+      replace: false,
+      template: '<div ng-click="">{{ ngLabel }}</md-button>',
+    };
+  });
