9e128ef1dcef100bc743728f8bc368828b32da74
Paul Gschwendtner <paulgschwendtner@me.com>
2016-02-06 17:14:30 +0100

    fix(travis): add access token for push permissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,13 @@ echo "Storing distribution files in home..."
 cp dist/ ~/dist -R
 rm dist/ -Rf
 
+echo "Cloning with private access token"
+git clone DevVersion-Bot:$GH_TOKEN@github.com/Werdenfels-Gymnasium/WG-Hilfe ~/WG-Hilfe-deploy/
+cd ~/WG-Hilfe-deploy
+
+echo "Fetching all"
+git fetch origin
+
 echo "Switching to gh-pages"
 git checkout gh-pages
 
