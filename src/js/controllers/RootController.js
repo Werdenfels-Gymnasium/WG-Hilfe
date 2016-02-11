779d1caeb8e49d7b917b1bbbb946a3b812cad779
Paul Thiel <lordnoname@t-online.de>
2016-02-11 19:32:07 +0100

    feat(): add loading spinner while loading guides
    update(): add $routechangestart and $routechangesuccess events and change loading spinner style
    fix(): remove trailing whitespaces
    Fixes #46

--- src/js/controllers/RootController.js
+++ src/js/controllers/RootController.js
@@ -1,12 +1,24 @@
 angular.module('wgHilfe')
-  .controller('RootController', RootController);
+  .controller('RootController', RootController)
+  .run(function($rootScope) {
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+      $rootScope.loadedContent = false;
+    });
+    $rootScope.$on('$routeChangeSuccess', function(event, next, current) {
+      $rootScope.loadedContent = true;
+    });
+  });
 
-function RootController($scope, $routeParams, GuideService) {
-
+function RootController($rootScope, $scope, $routeParams, GuideService) {
   GuideService.fetchData().then(function(data) {
+    $scope.loadedGuide = false;
     $scope.guides = data;
   }, function(error) {
     new Error('There occoured an error while reading the guides: ' + error);
+  }).then(function() {
+    $scope.loadedGuide = true;
   });
-
+  $rootScope.isLoaded = function() {
+    return (+ $rootScope.loadedContent & + $scope.loadedGuide) === 1 ? true : false;
+  }
 }
